880a10099e3c9ea4a045a769c0d1fb1698ffb5b7
John Roesler <john.roesler@bazaarvoice.com>
2016-09-09 17:20:30 -0500

    ability to run start-local.sh from any dir

--- yum/start-local.sh
+++ yum/start-local.sh
@@ -21,4 +21,15 @@
 # java -jar bin/emodb-web.jar server conf/config-local.yaml conf/config-ddl-local.yaml
 #
 
+#traverse all links to arrive at the real directory of the script.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+pushd $DIR
 java -jar bin/emodb-web-local-*.jar server conf/config-local.yaml conf/config-ddl-local.yaml conf/cassandra.yaml -z
+popd
