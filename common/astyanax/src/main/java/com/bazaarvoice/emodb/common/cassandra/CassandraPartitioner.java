16ae7e994273d7e0b5a3e418cdde58359935df27
Bill Kalter <bkalter@gmail.com>
2017-10-27 15:25:55 -0500

    EMO-6444: Changed partitioner validation to accept Emo BOP implementation (#127)
    * Changed partitioner validation to accept Emo BOP implementation

--- common/astyanax/src/main/java/com/bazaarvoice/emodb/common/cassandra/CassandraPartitioner.java
+++ common/astyanax/src/main/java/com/bazaarvoice/emodb/common/cassandra/CassandraPartitioner.java
@@ -39,7 +39,27 @@ public enum CassandraPartitioner {
 
         @Override
         public boolean matches(String cassandraPartitioner) {
-            return ByteOrderedPartitioner.class.getName().equals(cassandraPartitioner);
+            return ByteOrderedPartitioner.class.getName().equals(cassandraPartitioner) ||
+                    isEmoBOPImplementation(cassandraPartitioner);
+        }
+
+        /**
+         * EmoDB requires a partitioner whose token generation is identical to {@link ByteOrderedPartitioner}.
+         * However, the EmoDB team has found that ByteOrderedPartitioner itself has inefficiencies at scale unrelated to
+         * token management.  To address this the EmoDB team has created an alternate implementation, EmoPartitioner,
+         * which resolves these inefficiencies while otherwise behaving identically to BOP.  (As of this writing
+         * EmoPartitioner is not yet open sourced but this is the intention.)
+         *
+         * Currently EmoDB uses a mix of the native Cassandra driver and the thrift Astyanax driver.  Without going
+         * into the full details of why this is or why it would be difficult to abandon Asytanax completely, to support
+         * EmoPartitioner we would normally need to create a corresponding implementation of the Astyanax
+         * {@link Partitioner} for it.  However, Astyanax only requires the partitioner for a limited number of
+         * calls and for those the implementation would be identical to that in {@link BOP20Partitioner}  So if
+         * the Cassandra partitioner is EmoPartitioner then return BOP as an equivalent substitute.
+         *
+         */
+        private boolean isEmoBOPImplementation(String cassandraPartitioner) {
+            return "com.bazaarvoice.emodb.partitioner.EmoPartitioner".equals(cassandraPartitioner);
         }
     };
 
