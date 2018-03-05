6de8ca2dc0fbddc1faf7be806269fd6235a3abe3
billkalter <bill.kalter@bazaarvoice.com>
2018-03-05 13:39:03 -0600

    Removed legacy fanout

--- databus/src/main/java/com/bazaarvoice/emodb/databus/repl/DefaultReplicationManager.java
+++ databus/src/main/java/com/bazaarvoice/emodb/databus/repl/DefaultReplicationManager.java
@@ -19,13 +19,11 @@ import com.bazaarvoice.ostrich.pool.ServicePoolBuilder;
 import com.bazaarvoice.ostrich.pool.ServicePoolProxies;
 import com.bazaarvoice.ostrich.retry.ExponentialBackoffRetry;
 import com.codahale.metrics.MetricRegistry;
-import com.google.common.base.Supplier;
 import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.AbstractScheduledService;
 import com.google.common.util.concurrent.AbstractService;
-import com.google.common.util.concurrent.Service;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.inject.Inject;
 import com.sun.jersey.api.client.Client;
@@ -140,7 +138,6 @@ public class DefaultReplicationManager extends AbstractScheduledService {
         // Start asynchronously downloading events from the remote data center.
         final Managed fanout = new GuavaServiceController(_replicationEnabled, () -> new AbstractService() {
             Managed _fanout = null;
-            Managed _legacyFanout = null;
 
             @Override
             protected void doStart() {
@@ -148,7 +145,6 @@ public class DefaultReplicationManager extends AbstractScheduledService {
 
                 try {
                     _fanout.start();
-                    _legacyFanout.start();
                 } catch (Exception e) {
                     throw Throwables.propagate(e);
                 }
@@ -161,10 +157,6 @@ public class DefaultReplicationManager extends AbstractScheduledService {
                     if (_fanout != null) {
                         _fanout.stop();
                         _fanout = null;
-                    }
-                    if (_legacyFanout != null) {
-                        _legacyFanout.stop();
-                        _legacyFanout = null;
                     }
                 } catch (Exception e) {
                     throw Throwables.propagate(e);
