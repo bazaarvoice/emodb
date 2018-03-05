f634e323cc12ecb300b3b87dbe9b61f3ac5774e0
Ben Devore <ben.devore@bazaarvoice.com>
2018-03-05 15:19:34 -0500

    remove legacy table mutexes

--- table/src/main/java/com/bazaarvoice/emodb/table/db/curator/TableMutexManager.java
+++ table/src/main/java/com/bazaarvoice/emodb/table/db/curator/TableMutexManager.java
@@ -13,23 +13,18 @@ public class TableMutexManager {
     private static final int NUM_MUTEXES = 256;
 
     private final Mutex[] _mutexes;
-    private final CuratorMutex _oldMutex;
 
-    public TableMutexManager(CuratorFramework curator, String oldPath, String newPath) {
+    public TableMutexManager(CuratorFramework curator, String path) {
         _mutexes = new Mutex[NUM_MUTEXES];
         for (int i = 0; i < NUM_MUTEXES; i++) {
-            _mutexes[i] = new CuratorMutex(curator, ZKPaths.makePath(newPath, Integer.toString(i)));
+            _mutexes[i] = new CuratorMutex(curator, ZKPaths.makePath(path, Integer.toString(i)));
         }
-        _oldMutex = new CuratorMutex(curator, oldPath);
     }
 
     public void runWithLockForTable(Runnable runnable, Duration acquireTimeout, String table) {
 
-
-        _oldMutex.runWithLock(() -> {
-            _mutexes[Math.abs(Hashing.murmur3_128().hashString(table, Charsets.UTF_8).asInt() % NUM_MUTEXES)]
-                            .runWithLock(runnable, acquireTimeout);
-        }, acquireTimeout);
+        _mutexes[Math.abs(Hashing.murmur3_128().hashString(table, Charsets.UTF_8).asInt() % NUM_MUTEXES)]
+                .runWithLock(runnable, acquireTimeout);
 
     }
 }
