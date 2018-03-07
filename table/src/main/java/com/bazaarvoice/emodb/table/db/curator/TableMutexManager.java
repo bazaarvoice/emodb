bcbdb6d417a7d1007adc2f982e988701a542f92c
Ben Devore <bdevore@gwu.edu>
2018-03-07 11:59:29 -0500

    Remove legacy table mutex (#162)
    * remove legacy table mutexes

--- table/src/main/java/com/bazaarvoice/emodb/table/db/curator/TableMutexManager.java
+++ table/src/main/java/com/bazaarvoice/emodb/table/db/curator/TableMutexManager.java
@@ -4,32 +4,29 @@ import com.bazaarvoice.emodb.table.db.Mutex;
 import com.google.common.base.Charsets;
 import com.google.common.hash.Hashing;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.utils.ZKPaths;
 import org.joda.time.Duration;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class TableMutexManager {
 
     private static final int NUM_MUTEXES = 256;
 
     private final Mutex[] _mutexes;
-    private final CuratorMutex _oldMutex;
 
-    public TableMutexManager(CuratorFramework curator, String oldPath, String newPath) {
+    public TableMutexManager(CuratorFramework curator, String path) {
         _mutexes = new Mutex[NUM_MUTEXES];
         for (int i = 0; i < NUM_MUTEXES; i++) {
-            _mutexes[i] = new CuratorMutex(curator, ZKPaths.makePath(newPath, Integer.toString(i)));
+            _mutexes[i] = new CuratorMutex(curator, ZKPaths.makePath(path, Integer.toString(i)));
         }
-        _oldMutex = new CuratorMutex(curator, oldPath);
     }
 
     public void runWithLockForTable(Runnable runnable, Duration acquireTimeout, String table) {
-
-
-        _oldMutex.runWithLock(() -> {
-            _mutexes[Math.abs(Hashing.murmur3_128().hashString(table, Charsets.UTF_8).asInt() % NUM_MUTEXES)]
-                            .runWithLock(runnable, acquireTimeout);
-        }, acquireTimeout);
+        checkNotNull(table, "table cannot be null");
+        
+        _mutexes[Math.abs(Hashing.murmur3_128().hashString(table, Charsets.UTF_8).asInt() % NUM_MUTEXES)]
+                .runWithLock(runnable, acquireTimeout);
 
     }
 }
