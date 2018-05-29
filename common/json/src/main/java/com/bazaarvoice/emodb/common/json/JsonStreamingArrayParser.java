25531943e0ab297dc1dd63ca75cd480c1047f7b7
billkalter <bill.kalter@bazaarvoice.com>
2018-05-29 11:16:54 -0500

    Removed Guava dependency from emodb-stash-common

--- common/json/src/main/java/com/bazaarvoice/emodb/common/json/JsonStreamingArrayParser.java
+++ common/json/src/main/java/com/bazaarvoice/emodb/common/json/JsonStreamingArrayParser.java
@@ -1,5 +1,7 @@
 package com.bazaarvoice.emodb.common.json;
 
+import com.bazaarvoice.emodb.streaming.AbstractSpliterator;
+import com.bazaarvoice.emodb.streaming.SpliteratorIterator;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -13,22 +15,16 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
-import java.util.Iterator;
 import java.util.Spliterator;
-import java.util.Spliterators;
-import java.util.function.Consumer;
-import java.util.stream.StreamSupport;
 
 /**
  * Incrementally parses an JSON array of objects, returning the results as an iterator.  Allows parsing an
  * arbitrary amount of data, potentially more than could fit in memory at one time.
  */
-public class JsonStreamingArrayParser<T> implements Iterator<T>, Closeable {
+public class JsonStreamingArrayParser<T> extends SpliteratorIterator<T> implements Closeable {
     private final JsonParser _jp;
     private final ObjectReader _reader;
     private final Class<? extends T> _type;
-    private final Iterator<T> _finalIter;
-    private boolean _endOfStream;
 
     public JsonStreamingArrayParser(InputStream in, Class<? extends T> elementType) {
         this(in, CustomJsonObjectMapperFactory.build(), elementType);
@@ -64,58 +60,35 @@ public class JsonStreamingArrayParser<T> implements Iterator<T>, Closeable {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        _finalIter = StreamSupport.stream(asSpliterator(), false).iterator();
     }
 
     @Override
-    public boolean hasNext() {
-        return _finalIter.hasNext();
-    }
-
-    @Override
-    public T next() {
-        return _finalIter.next();
-    }
-
-    private Spliterator<T> asSpliterator() {
-        return new Spliterators.AbstractSpliterator<T>(Long.MAX_VALUE, 0) {
+    protected Spliterator<T> getSpliterator() {
+        return new AbstractSpliterator<T>() {
             @Override
-            public boolean tryAdvance(Consumer<? super T> action) {
-                if (!_endOfStream) {
-                    T next = computeNext();
-                    if (!_endOfStream) {
-                        action.accept(next);
-                        return true;
+            protected T computeNext() {
+                try {
+                    if (_jp.nextToken() == JsonToken.END_ARRAY) {
+                        if (_jp.nextToken() != null) {
+                            throw new IllegalStateException("Expected EOF in JavaScript input stream.");
+                        }
+                        _jp.close();
+                        return endOfStream();
                     }
+                    return _type.cast(_reader.readValue(_jp));
+                } catch (IOException e) {
+                    // If the root cause is a JsonProcessingException then throw it as a JsonStreamProcessingException.
+                    if (isJsonProcessingException(e)) {
+                        throw new JsonStreamProcessingException(e);
+                    }
+
+                    // We already parsed the first few bytes in the constructor and verified that the InputStream looked valid
+                    // so if there's an unexpected end of input here it likely means we lost the connection to the server.
+                    // In practice this a JsonStreamingEOFException or a TruncatedChunkException or something similar.
+                    throw new JsonStreamingEOFException(e);
                 }
-                return false;
             }
         };
-    }
-
-    private T computeNext() {
-        try {
-            if (_jp.nextToken() == JsonToken.END_ARRAY) {
-                if (_jp.nextToken() != null) {
-                    throw new IllegalStateException("Expected EOF in JavaScript input stream.");
-                }
-                _jp.close();
-                _endOfStream = true;
-                return null;
-            }
-            return _type.cast(_reader.readValue(_jp));
-        } catch (IOException e) {
-            // If the root cause is a JsonProcessingException then throw it as a JsonStreamProcessingException.
-            if (isJsonProcessingException(e)) {
-                throw new JsonStreamProcessingException(e);
-            }
-
-            // We already parsed the first few bytes in the constructor and verified that the InputStream looked valid
-            // so if there's an unexpected end of input here it likely means we lost the connection to the server.
-            // In practice this a JsonStreamingEOFException or a TruncatedChunkException or something similar.
-            throw new JsonStreamingEOFException(e);
-        }
     }
 
     /**
--- common/json/src/main/java/com/bazaarvoice/emodb/common/json/RestartingStreamingIterator.java
+++ common/json/src/main/java/com/bazaarvoice/emodb/common/json/RestartingStreamingIterator.java
@@ -1,5 +1,7 @@
 package com.bazaarvoice.emodb.common.json;
 
+import com.bazaarvoice.emodb.streaming.AbstractSpliterator;
+import com.bazaarvoice.emodb.streaming.SpliteratorIterator;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
@@ -7,21 +9,16 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Spliterator;
-import java.util.Spliterators;
-import java.util.function.Consumer;
-import java.util.stream.StreamSupport;
 
 /**
  * Wraps a {@link JsonStreamingArrayParser} and detects early EOF and re-starts the streaming just after the last
  * object successfully parsed.
  */
-public class RestartingStreamingIterator<T, K> implements Iterator<T>, Closeable {
+public class RestartingStreamingIterator<T, K> extends SpliteratorIterator<T> implements Closeable {
     private final StreamingIteratorSupplier<T, K> _iterSupplier;
-    private final Iterator<T> _finalIter;
     private K _restartFromToken;
     private long _remaining;
     private Iterator<T> _iter;
-    private boolean _endOfStream;
 
     public static <T, K> Iterable<T> stream(final @Nullable K fromToken,
                                             final long limit,
@@ -41,75 +38,52 @@ public class RestartingStreamingIterator<T, K> implements Iterator<T>, Closeable
             // Expected to be a JsonStreamingArrayParser
             throw new IllegalArgumentException("Iterator returned should be closeable");
         };
-        _finalIter = StreamSupport.stream(asRestartingSpliterator(), false).iterator();
     }
 
     @Override
-    public boolean hasNext() {
-        return _finalIter.hasNext();
-    }
-
-    @Override
-    public T next() {
-        return _finalIter.next();
-    }
-
-    private Spliterator<T> asRestartingSpliterator() {
-        return new Spliterators.AbstractSpliterator<T>(Long.MAX_VALUE, 0) {
+    protected Spliterator<T> getSpliterator() {
+        return new AbstractSpliterator<T>() {
             @Override
-            public boolean tryAdvance(Consumer<? super T> action) {
-                if (!_endOfStream) {
-                    T next = computeNext();
-                    if (!_endOfStream) {
-                        action.accept(next);
-                        return true;
+            protected T computeNext() {
+                boolean retrying = false;
+                while (_remaining > 0) {
+                    // All calls to the iterator must be protected by a try/catch for JsonStreamingEOFException.
+                    T next;
+                    try {
+                        if (!_iter.hasNext()) {
+                            // swallow any exceptions since we don't care if we can't close input data
+                            closeUnchecked((Closeable) _iter);
+                            return endOfStream();
+                        }
+                        next = _iter.next();
+                    } catch (JsonStreamingEOFException e) {
+                        // Lost the underlying input stream.
+                        // swallow any exceptions since we don't care if we can't close a bad stream
+                        closeUnchecked((Closeable) _iter);
+
+                        // When restarting the iterator ensure we retrieve at least one value successfully to avoid the risk
+                        // of entering an infinite retry loop.
+                        if (retrying) {
+                            throw e;
+                        }
+                        retrying = true;
+
+                        // This next call may fail.  If it does, let the exception propagate.  We assume it implements
+                        // the retry logic appropriate for re-creating the stream of input values.
+                        _iter = _iterSupplier.get(_restartFromToken, _remaining);
+                        continue;
                     }
+
+                    _restartFromToken = _iterSupplier.getNextToken(next);
+                    _remaining--;
+
+                    return next;
                 }
-                return false;
+                // swallow any exceptions since we don't care if we can't close input data
+                closeUnchecked((Closeable) _iter);
+                return endOfStream();
             }
         };
-    }
-
-    private T computeNext() {
-        boolean retrying = false;
-        while (_remaining > 0) {
-            // All calls to the iterator must be protected by a try/catch for JsonStreamingEOFException.
-            T next;
-            try {
-                if (!_iter.hasNext()) {
-                    // swallow any exceptions since we don't care if we can't close input data
-                    closeUnchecked((Closeable) _iter);
-                    _endOfStream = true;
-                    return null;
-                }
-                next = _iter.next();
-            } catch (JsonStreamingEOFException e) {
-                // Lost the underlying input stream.
-                // swallow any exceptions since we don't care if we can't close a bad stream
-                closeUnchecked((Closeable) _iter);
-
-                // When restarting the iterator ensure we retrieve at least one value successfully to avoid the risk
-                // of entering an infinite retry loop.
-                if (retrying) {
-                    throw e;
-                }
-                retrying = true;
-
-                // This next call may fail.  If it does, let the exception propagate.  We assume it implements
-                // the retry logic appropriate for re-creating the stream of input values.
-                _iter = _iterSupplier.get(_restartFromToken, _remaining);
-                continue;
-            }
-
-            _restartFromToken = _iterSupplier.getNextToken(next);
-            _remaining--;
-
-            return next;
-        }
-        // swallow any exceptions since we don't care if we can't close input data
-        closeUnchecked((Closeable) _iter);
-        _endOfStream = true;
-        return null;
     }
 
     private void closeUnchecked(Closeable closeable) {
