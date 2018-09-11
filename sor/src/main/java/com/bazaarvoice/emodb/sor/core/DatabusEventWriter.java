f2e05aee1b4430e896d0cace9ba7fba72ea90e5d
Ben Devore <ben.devore@bazaarvoice.com>
2018-09-11 15:10:51 -0500

    add comments

--- sor/src/main/java/com/bazaarvoice/emodb/sor/core/DatabusEventWriter.java
+++ sor/src/main/java/com/bazaarvoice/emodb/sor/core/DatabusEventWriter.java
@@ -1,5 +1,12 @@
 package com.bazaarvoice.emodb.sor.core;
 
+/**
+ * Interface that should register with {@link DatabusEventWriterRegistry} and write events to the databus prior to
+ * writing them to the system of record. If an event fails to be written to the databus, then it should propagate and
+ * exception and prevent Emo from writing to the system of record.
+ */
 public interface DatabusEventWriter {
+
+
     void writeEvent(UpdateIntentEvent event);
 }
--- sor/src/main/java/com/bazaarvoice/emodb/sor/core/DatabusEventWriterRegistry.java
+++ sor/src/main/java/com/bazaarvoice/emodb/sor/core/DatabusEventWriterRegistry.java
@@ -3,6 +3,12 @@ package com.bazaarvoice.emodb.sor.core;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * This is used to be used to send events to the databus prior to writing to the system of record.
+ * This databus should register as an {@link DatabusEventWriter} with this registry and then take events synchronously.
+ * If an event fails to be written to the databus, then it should propagate and exception and prevent Emo from writing
+ * to the system of record.
+ */
 public class DatabusEventWriterRegistry {
 
     private DatabusEventWriter _databusWriter;
@@ -22,9 +28,5 @@ public class DatabusEventWriterRegistry {
 
     public DatabusEventWriter getDatabusWriter() {
         return _databusWriter;
-    }
-
-    public boolean hasRegistered() {
-        return _hasRegistered;
     }
 }
