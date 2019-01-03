5be6e91c10278cecf0692c8683cc70f954b4fc1a
Mike Unitskyi <mykhailo.unitskyi@bazaarvoice.com>
2019-01-03 18:38:33 +0200

    changes according to code review

--- web-local/start-zookeeper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Docker Installation on Mac OS X: https://docs.docker.com/engine/installation/mac/
-#
-# Starts the following services locally (see configuration reference https://docs.docker.com/samples/library/zookeeper/):
-# - ZooKeeper (port 2181)
-
-docker run -p 2181:2181  -v $(pwd)/target/zookeeper:/data zookeeper:3.4.13
