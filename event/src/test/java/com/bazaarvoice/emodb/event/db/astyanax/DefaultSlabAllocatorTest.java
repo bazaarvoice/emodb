6dbc88f4b5b8ab578103dfa331aa639878c9fb57
michael-card <mike.card@bazaarvoice.com>
2019-01-07 16:17:21 -0600

    EMO-6802: Add maximum size in bytes for a slab tp prevent problems due to very large documents
    EMO-6802: incorporated review comments
    
    EMO-6802: Add logic for edge cases found during stress tests
    
    EMO-6802: incorporate next round of CR comments
    
    EMO-6802: Added unit tests fixed bug in ChannelAllocationStatde class
    
    EMO-6802: Remove extraneous byte allocation check from allocate method
    
    EMO-6802: Incorporated review comments
    
    Fix some issues identified by Sonarqube
    
    EMO-6802: Incorporate review comment on matching iterator length to allocation size
    
    EMO-6802: correct eventIdx to be zero-based and re-test
    
    EMO-6802: correct error in slab offset computation and fix default allocation count  to return allocation info by itself not allocation+already consumed slab info
    
    EMO-6802: Remove logger
    
    EMO-6802: Incorporate CR comments
    
    EMO-6802: unit test improvements

---
+++ event/src/test/java/com/bazaarvoice/emodb/event/db/astyanax/DefaultSlabAllocatorTest.java
@@ -0,0 +1,165 @@
+package com.bazaarvoice.emodb.event.db.astyanax;
+
+import com.bazaarvoice.emodb.common.dropwizard.lifecycle.LifeCycleRegistry;
+import com.codahale.metrics.MetricRegistry;
+import com.google.common.collect.Iterators;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.testng.Assert.assertTrue;
+
+public class DefaultSlabAllocatorTest {
+
+    private static final Logger _log = LoggerFactory.getLogger(DefaultSlabAllocatorTest.class);
+
+    @Test
+    public void allocateGreaterThanMaxEvents() {
+        List<Integer> sizes = new ArrayList<Integer>();
+        for (int i=0; i<Constants.MAX_SLAB_SIZE*3/2; i++) {
+            sizes.add(64);
+        }
+        DefaultSlabAllocator slabAllocator = new DefaultSlabAllocator(mock(LifeCycleRegistry.class), mock(ManifestPersister.class), "metrics", mock(MetricRegistry.class));
+        try {
+            slabAllocator.allocate("mychannel", Constants.MAX_SLAB_SIZE*3/2, Iterators.peekingIterator(sizes.iterator()));
+            _log.info("SUCCESS: No exception thrown when " + (Constants.MAX_SLAB_SIZE*3/2) + " events allocated");
+        } catch (Exception e) {
+            _log.error("ERROR: " + e.getClass().getName() + " thrown when " + (Constants.MAX_SLAB_SIZE*3/2) + " events allocated");
+            assert(false);
+        }
+    }
+
+    @Test
+    public void allocateMaxEvents() {
+        List<Integer> sizes = new ArrayList<Integer>();
+        for (int i=0; i<Constants.MAX_SLAB_SIZE; i++) {
+            sizes.add(64);
+        }
+        DefaultSlabAllocator slabAllocator = new DefaultSlabAllocator(mock(LifeCycleRegistry.class), mock(ManifestPersister.class), "metrics", mock(MetricRegistry.class));
+        try {
+            slabAllocator.allocate("mychannel", Constants.MAX_SLAB_SIZE, Iterators.peekingIterator(sizes.iterator()));
+            _log.info("SUCCESS: No exception thrown when " + Constants.MAX_SLAB_SIZE + " events allocated");
+        } catch (Exception e) {
+            _log.error("ERROR: " + e.getClass().getName() + " thrown when " + Constants.MAX_SLAB_SIZE + " events allocated");
+            assert(false);
+        }
+    }
+
+    @Test
+    public void allocateFewerThanMaxEvents() {
+        List<Integer> sizes = new ArrayList<Integer>();
+        for (int i=0; i<Constants.MAX_SLAB_SIZE/2; i++) {
+            sizes.add(64);
+        }
+        DefaultSlabAllocator slabAllocator = new DefaultSlabAllocator(mock(LifeCycleRegistry.class), mock(ManifestPersister.class), "metrics", mock(MetricRegistry.class));
+        try {
+            slabAllocator.allocate("mychannel", Constants.MAX_SLAB_SIZE/2, Iterators.peekingIterator(sizes.iterator()));
+            _log.info("SUCCESS: No exception thrown when " + (Constants.MAX_SLAB_SIZE/2) + " events allocated");
+        } catch (Exception e) {
+            _log.error("ERROR: " + e.getClass().getName() + " thrown when " + (Constants.MAX_SLAB_SIZE/2) + " events allocated");
+            assert(false);
+        }
+    }
+
+
+    @Test
+    public void computeAllocationGreaterThanMaxEvents() {
+        List<Integer> sizes = new ArrayList<Integer>();
+        for (int i=0; i<Constants.MAX_SLAB_SIZE*3/2; i++) {
+            sizes.add(64);
+        }
+        try {
+            Pair<Integer, Integer> allocation = DefaultSlabAllocator.defaultAllocationCount(0,0, Iterators.peekingIterator(sizes.iterator()));
+            assertTrue(allocation.getLeft() == Constants.MAX_SLAB_SIZE, "ERROR: allocation has " + allocation.getLeft() + " slots, " + Constants.MAX_SLAB_SIZE + " expected");
+            _log.info("SUCCESS: allocation has " + allocation.getLeft() + " slots, " + Constants.MAX_SLAB_SIZE + " expected when " + 0 + " slots already used and " + (Constants.MAX_SLAB_SIZE*3/2) + " requested");
+        } catch (Exception e) {
+            _log.error("ERROR: " + e.getClass().getName() + " thrown when " + (Constants.MAX_SLAB_SIZE*3/2) + " events allocated");
+            assert(false);
+        }
+    }
+
+    @Test
+    public void computeAllocationMaxEvents() {
+        List<Integer> sizes = new ArrayList<Integer>();
+        for (int i=0; i<Constants.MAX_SLAB_SIZE; i++) {
+            sizes.add(64);
+        }
+        try {
+            Pair<Integer, Integer> allocation = DefaultSlabAllocator.defaultAllocationCount(0,0, Iterators.peekingIterator(sizes.iterator()));
+            assertTrue(allocation.getLeft() == Constants.MAX_SLAB_SIZE, "ERROR: allocation has " + allocation.getLeft() + " slots, " + Constants.MAX_SLAB_SIZE + " expected");
+            _log.info("SUCCESS: allocation has " + allocation.getLeft() + " slots, " + Constants.MAX_SLAB_SIZE + " expected when " + 0 + " slots already used and " + Constants.MAX_SLAB_SIZE + " requested");
+        } catch (Exception e) {
+            _log.error("ERROR: " + e.getClass().getName() + " thrown when " + Constants.MAX_SLAB_SIZE + " events allocated");
+            assert(false);
+        }
+    }
+
+    @Test
+    public void computeAllocationFewerThanMaxEvents() {
+        List<Integer> sizes = new ArrayList<Integer>();
+        for (int i=0; i<Constants.MAX_SLAB_SIZE/2; i++) {
+            sizes.add(64);
+        }
+        try {
+            Pair<Integer, Integer> allocation = DefaultSlabAllocator.defaultAllocationCount(0,0, Iterators.peekingIterator(sizes.iterator()));
+            assertTrue(allocation.getLeft() == Constants.MAX_SLAB_SIZE/2, "ERROR: allocation has " + allocation.getLeft() + " slots, " + (Constants.MAX_SLAB_SIZE/2) + " expected");
+            _log.info("SUCCESS: allocation has " + allocation.getLeft() + " slots, " + (Constants.MAX_SLAB_SIZE/2) + " expected when " + 0 + " slots already used and " + (Constants.MAX_SLAB_SIZE/2) + " requested");
+        } catch (Exception e) {
+            _log.error("ERROR: " + e.getClass().getName() + " thrown when " + (Constants.MAX_SLAB_SIZE/2) + " events allocated");
+            assert(false);
+        }
+    }
+
+    @Test
+    public void computeAllocationTotalMaxEvents() {
+        List<Integer> sizes = new ArrayList<Integer>();
+        for (int i=0; i<Constants.MAX_SLAB_SIZE/2; i++) {
+            sizes.add(64);
+        }
+        try {
+            Pair<Integer, Integer> allocation = DefaultSlabAllocator.defaultAllocationCount(Constants.MAX_SLAB_SIZE/2,0, Iterators.peekingIterator(sizes.iterator()));
+            assertTrue(allocation.getLeft() == Constants.MAX_SLAB_SIZE/2, "ERROR: allocation has " + allocation.getLeft() + " slots, " + (Constants.MAX_SLAB_SIZE/2) + " expected");
+            _log.info("SUCCESS: allocation has " + allocation.getLeft() + " slots, " + (Constants.MAX_SLAB_SIZE/2) + " expected when " + (Constants.MAX_SLAB_SIZE/2) + " slots already used and " + Constants.MAX_SLAB_SIZE/2 + " requested");
+        } catch (Exception e) {
+            _log.error("ERROR: " + e.getClass().getName() + " thrown when " + (Constants.MAX_SLAB_SIZE/2) + " events allocated");
+            assert(false);
+        }
+    }
+
+    @Test
+    public void computeAllocationTotalGreaterThanMaxEvents() {
+        List<Integer> sizes = new ArrayList<Integer>();
+        for (int i=0; i<Constants.MAX_SLAB_SIZE; i++) {
+            sizes.add(64);
+        }
+        try {
+            Pair<Integer, Integer> allocation = DefaultSlabAllocator.defaultAllocationCount(Constants.MAX_SLAB_SIZE/2,0, Iterators.peekingIterator(sizes.iterator()));
+            assertTrue(allocation.getLeft() == Constants.MAX_SLAB_SIZE/2, "ERROR: allocation has " + allocation.getLeft() + " slots, " + (Constants.MAX_SLAB_SIZE/2) + " expected");
+            _log.info("SUCCESS: allocation has " + allocation.getLeft() + " slots, " + (Constants.MAX_SLAB_SIZE/2) + " expected when " + (Constants.MAX_SLAB_SIZE/2) + " slots already used and " + Constants.MAX_SLAB_SIZE + " requested");
+        } catch (Exception e) {
+            _log.error("ERROR: " + e.getClass().getName() + " thrown when " + (Constants.MAX_SLAB_SIZE/2) + " events allocated");
+            assert(false);
+        }
+    }
+
+    @Test
+    public void computeAllocationTotalGreaterThanMaxEventsWithOversizeEvent() {
+        List<Integer> sizes = new ArrayList<Integer>();
+        for (int i=0; i<Constants.MAX_SLAB_SIZE-1; i++) {
+            sizes.add(64);
+        }
+        sizes.add(Constants.MAX_EVENT_SIZE_IN_BYTES+1);
+        try {
+            Pair<Integer, Integer> allocation = DefaultSlabAllocator.defaultAllocationCount(Constants.MAX_SLAB_SIZE/2,0, Iterators.peekingIterator(sizes.iterator()));
+            _log.error("ERROR: allocation did not throw exceptoion for oversize event");
+        } catch (Exception e) {
+            _log.info("SUCCESS: " + e.getClass().getName() + " thrown when " + (Constants.MAX_SLAB_SIZE/2) + " events allocated");
+        }
+    }
+
+}
