9d73b4e1c2b1344560e2c9f0b321cfb695d52164
Ben Devore <ben.devore@bazaarvoice.com>
2019-01-28 13:21:02 -0600

    upgrade jersey emo client

--- common/dropwizard-client/src/main/java/com/bazaarvoice/emodb/common/jersey/dropwizard/JerseyEmoResponse.java
+++ common/dropwizard-client/src/main/java/com/bazaarvoice/emodb/common/jersey/dropwizard/JerseyEmoResponse.java
@@ -3,26 +3,27 @@ package com.bazaarvoice.emodb.common.jersey.dropwizard;
 import com.bazaarvoice.emodb.client.EmoResponse;
 import com.bazaarvoice.emodb.client.EntityHelper;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.sun.jersey.api.client.ClientResponse;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.Objects.requireNonNull;
+
 
 /**
- * EmoResponse implementation that wraps a Jersey {@link ClientResponse}.
+ * EmoResponse implementation that wraps a Jersey {@link Response}.
  */
 public class JerseyEmoResponse implements EmoResponse {
 
-    private final ClientResponse _response;
+    private final Response _response;
 
-    public JerseyEmoResponse(ClientResponse response) {
-        _response = checkNotNull(response, "response");
+    public JerseyEmoResponse(Response response) {
+        _response = requireNonNull(response, "response");
     }
 
     @Override
@@ -32,17 +33,17 @@ public class JerseyEmoResponse implements EmoResponse {
 
     @Override
     public List<String> getHeader(String header) {
-        return _response.getHeaders().get(header);
+        return _response.getStringHeaders().get(header);
     }
 
     @Override
     public String getFirstHeader(String header) {
-        return _response.getHeaders().getFirst(header);
+        return _response.getStringHeaders().getFirst(header);
     }
 
     @Override
     public Iterable<Map.Entry<String, List<String>>> getHeaders() {
-        return _response.getHeaders().entrySet();
+        return _response.getStringHeaders().entrySet();
     }
 
     @Override
@@ -52,16 +53,16 @@ public class JerseyEmoResponse implements EmoResponse {
 
     @Override
     public InputStream getEntityInputStream() {
-        return _response.getEntityInputStream();
+        return _response.readEntity(InputStream.class);
     }
 
     @Override
     public <T> T getEntity(Class<T> clazz) {
         // Don't attempt to read the entity as JSON unless the header indicates to do so.
-        if (MediaType.APPLICATION_JSON_TYPE.equals(_response.getType())) {
+        if (MediaType.APPLICATION_JSON_TYPE.equals(_response.getMediaType())) {
             return EntityHelper.getEntity(getEntityInputStream(), clazz);
         }
-        return _response.getEntity(clazz);
+        return _response.readEntity(clazz);
     }
 
     @Override
