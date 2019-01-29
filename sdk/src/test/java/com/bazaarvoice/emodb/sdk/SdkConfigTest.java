289e3223c7a7e052fc175aa9638a1235a214f465
Ben Devore <ben.devore@bazaarvoice.com>
2019-01-29 15:28:01 -0600

    refactor sdk

--- sdk/src/test/java/com/bazaarvoice/emodb/sdk/SdkConfigTest.java
+++ sdk/src/test/java/com/bazaarvoice/emodb/sdk/SdkConfigTest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import io.dropwizard.configuration.ConfigurationException;
 import io.dropwizard.configuration.ConfigurationFactory;
+import io.dropwizard.configuration.YamlConfigurationFactory;
 import org.testng.annotations.Test;
 
 import javax.validation.Validation;
@@ -21,7 +22,7 @@ public class SdkConfigTest {
         // This test makes sure that we haven't forgotten to update the emodb sdk default config file when we add/remove properties
         Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
         ObjectMapper mapper = EmoServiceObjectMapperFactory.build(new YAMLFactory());
-        ConfigurationFactory configurationFactory = new ConfigurationFactory(EmoConfiguration.class, validator, mapper, "dw");
+        ConfigurationFactory configurationFactory = new YamlConfigurationFactory(EmoConfiguration.class, validator, mapper, "dw");
         // Make sure that our config files are up to date
         configurationFactory.build(
         new File(EmoStartMojo.class.getResource("/emodb-default-config.yaml").toURI()));
