63052e340db1d9758550ad9b2d3cd81802d7614c
Ben Devore <ben.devore@bazaarvoice.com>
2019-02-15 13:11:37 -0600

    fix isRetriableExpception

--- sor-client/src/main/java/com/bazaarvoice/emodb/sor/client/DataStoreClientFactory.java
+++ sor-client/src/main/java/com/bazaarvoice/emodb/sor/client/DataStoreClientFactory.java
@@ -2,9 +2,13 @@ package com.bazaarvoice.emodb.sor.client;
 
 import com.bazaarvoice.emodb.common.jersey.dropwizard.JerseyEmoClient;
 import com.codahale.metrics.MetricRegistry;
+import com.google.common.base.Predicates;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Iterables;
 import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.util.Duration;
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
 
@@ -24,9 +28,9 @@ public class DataStoreClientFactory extends AbstractDataStoreClientFactory {
 
     @Override
     public boolean isRetriableException(Exception e) {
-        // TODO: explore if the removal of ClientHandlerException is acceptable
         return super.isRetriableException(e) ||
                 (e instanceof WebApplicationException &&
-                        ((WebApplicationException) e).getResponse().getStatus() >= 500);
+                        ((WebApplicationException) e).getResponse().getStatus() >= 500) ||
+                Iterables.any(Throwables.getCausalChain(e), Predicates.instanceOf(ProcessingException.class));
     }
 }
