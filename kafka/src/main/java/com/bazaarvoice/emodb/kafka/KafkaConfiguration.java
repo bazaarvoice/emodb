1570ed9ba4ddada762931c0cb3fff5a88e7dd7f0
Ben Devore <ben.devore@bazaarvoice.com>
2019-09-04 13:07:46 -0500

    make megabus kafka producer configurable

--- kafka/src/main/java/com/bazaarvoice/emodb/kafka/KafkaConfiguration.java
+++ kafka/src/main/java/com/bazaarvoice/emodb/kafka/KafkaConfiguration.java
@@ -11,7 +11,16 @@ public class KafkaConfiguration {
     @JsonProperty("bootstrapServers")
     private String _kafkaBootstrapServers;
 
+    @Valid
+    @NotNull
+    @JsonProperty("producer")
+    private KafkaProducerConfiguration _kafkaProducerConfiguration = new KafkaProducerConfiguration();
+
     public String getBootstrapServers() {
+        return _kafkaBootstrapServers;
+    }
+
+    public String getKafkaBootstrapServers() {
         return _kafkaBootstrapServers;
     }
 }
