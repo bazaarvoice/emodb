2ad942574f39bce5f8d9d93a9ccfa519bb9e8a51
Mike Unitskyi <mike-unitskyi@users.noreply.github.com>
2019-12-12 16:55:08 +0200

    EMO-7191 cache blob s3 healthchecks (#290)

--- blob/src/main/java/com/bazaarvoice/emodb/blob/db/s3/S3HealthCheck.java
+++ blob/src/main/java/com/bazaarvoice/emodb/blob/db/s3/S3HealthCheck.java
@@ -4,23 +4,44 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ListObjectsV2Request;
 import com.bazaarvoice.emodb.common.dropwizard.healthcheck.HealthCheckRegistry;
 import com.codahale.metrics.health.HealthCheck;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 
 import javax.inject.Inject;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class S3HealthCheck extends HealthCheck {
     private final Map<String, AmazonS3> _bucketNamesToS3Clients;
+    private final S3HealthCheckConfiguration _healthCheckConfiguration;
+    private final LoadingCache<String, Result> _healthCheckCache;
 
     @Inject
     public S3HealthCheck(@S3BucketNamesToS3Clients Map<String, AmazonS3> bucketNamesToS3Clients,
+                         S3HealthCheckConfiguration s3HealthCheckConfiguration,
                          HealthCheckRegistry healthCheckRegistry) {
         _bucketNamesToS3Clients = Objects.requireNonNull(bucketNamesToS3Clients);
-        healthCheckRegistry.addHealthCheck("blob-s3", this);
+        _healthCheckConfiguration = Objects.requireNonNull(s3HealthCheckConfiguration);
+        _healthCheckCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(_healthCheckConfiguration.getDuration().getSeconds(), TimeUnit.SECONDS)
+                .build(new CacheLoader<String, Result>() {
+                    @Override
+                    public Result load(String key) throws Exception {
+                        return getResult();
+                    }
+                });
+
+        healthCheckRegistry.addHealthCheck(_healthCheckConfiguration.getName(), this);
     }
 
     @Override
     protected Result check() throws Exception {
+        return _healthCheckCache.getUnchecked(_healthCheckConfiguration.getName());
+    }
+
+    private Result getResult() {
         return _bucketNamesToS3Clients.entrySet().parallelStream()
                 .map(entry -> {
                     try {
@@ -31,9 +52,9 @@ public class S3HealthCheck extends HealthCheck {
                     } catch (Exception e) {
                         return Result.unhealthy("Bucket " + entry.getKey() + " is unhealthy: " + e.getMessage());
                     }
-                }).reduce((result, result2) -> {
-                    String message = result.getMessage() + ", " + result2.getMessage();
-                    if (result.isHealthy() && result2.isHealthy()) {
+                }).reduce((result1, result2) -> {
+                    String message = result1.getMessage() + ", " + result2.getMessage();
+                    if (result1.isHealthy() && result2.isHealthy()) {
                         return Result.healthy(message);
                     } else {
                         return Result.unhealthy(message);
