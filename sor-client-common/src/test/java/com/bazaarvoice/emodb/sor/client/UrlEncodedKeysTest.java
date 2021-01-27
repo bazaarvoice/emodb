c3affe4181e0760b6917d9218a2a6d038e97769b
Mike Unitskyi <mike.unitsky@gmail.com>
2021-01-27 13:20:04 +0200

    fix test

--- sor-client-common/src/test/java/com/bazaarvoice/emodb/sor/client/UrlEncodedKeysTest.java
+++ sor-client-common/src/test/java/com/bazaarvoice/emodb/sor/client/UrlEncodedKeysTest.java
@@ -25,7 +25,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class UrlEncodedKeysTest {
-   /* private EmoClient _client = mock(EmoClient.class);
+    private EmoClient _client = mock(EmoClient.class);
 
     @BeforeMethod
     public void setUp() {
@@ -109,5 +109,5 @@ public class UrlEncodedKeysTest {
                 return format("http://%s/%s/%s?...", host, table, encodedKey);
             }
         };
-    }*/
+    }
 }
