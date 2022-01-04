150fe6729dea3246204248c59914e91d9d898647
Ashwini Sheshadri <79937630+ashwini-sheshadri@users.noreply.github.com>
2022-01-04 07:16:58 -0800

    PD 3339 Jersey2 Client for Databus (#391)
    * Add Databus client
    
    * various fixes
    
    * updated RetryPolicy library usages
    
    * small fix
    
    * dependency fix
    
    * Trigger Build
    
    * new line
    
    * Databus client implementation changes
    
    * fix maven analyzer issues
    
    * Trigger Build
    
    * Updated DatabusFactory parameters, code format and logging
    
    * extract client
    
    * separate Service discovery implementations
    
    * various improvements
    
    * update emodb version in new module of databus client
    
    * updated project version
    
    * updated project version
    
    * code cleanup
    
    * cleanup
    
    * use client2 module
    
    * use Objects.requireNonNull
    
    * replace deprecated
    
    * version updated
    
    Co-authored-by: Mike Unitskyi <mike.unitsky@gmail.com>

---
+++ databus-client2/src/main/java/com/bazaarvoice/emodb/databus/client2/Json/JsonUtil.java
@@ -0,0 +1,74 @@
+package com.bazaarvoice.emodb.databus.client2.Json;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.google.common.base.Throwables;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class JsonUtil {
+
+    private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+            .registerModule(new Jdk8Module())
+            .registerModule(new JavaTimeModule());
+
+    public static <T> T parseJson(String json, Class<T> type) {
+        try {
+            return OBJECT_MAPPER.readValue(json, type);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T parseJson(byte[] json, Class<T> type) {
+        try {
+            return OBJECT_MAPPER.readValue(json, type);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T parseJson(InputStream json, Class<T> type) {
+        try {
+            return OBJECT_MAPPER.readValue(json, type);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T parseJson(String json, TypeReference<T> type) {
+        try {
+            return OBJECT_MAPPER.readValue(json, type);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T parseJson(byte[] json, TypeReference<T> type) {
+        try {
+            return OBJECT_MAPPER.readValue(json, type);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T parseJson(InputStream json, TypeReference<T> type) {
+        try {
+            return OBJECT_MAPPER.readValue(json, type);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String toJsonString(Object value) {
+        try {
+            return OBJECT_MAPPER.writeValueAsString(value);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
