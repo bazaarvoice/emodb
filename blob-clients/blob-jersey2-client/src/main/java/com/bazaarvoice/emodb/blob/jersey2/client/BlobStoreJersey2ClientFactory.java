5740c50bb563486cef97b846cbef20ee44901878
ashwini-sheshadri <ashwini.sheshadri@bazaarvoice.com>
2022-04-27 17:59:59 -0700

    Update Blobstore client to use RetryPolicy from common module

--- blob-clients/blob-jersey2-client/src/main/java/com/bazaarvoice/emodb/blob/jersey2/client/BlobStoreJersey2ClientFactory.java
+++ blob-clients/blob-jersey2-client/src/main/java/com/bazaarvoice/emodb/blob/jersey2/client/BlobStoreJersey2ClientFactory.java
@@ -1,12 +1,8 @@
 package com.bazaarvoice.emodb.blob.jersey2.client;
 
 import com.bazaarvoice.emodb.common.jersey2.Jersey2EmoClient;
-import com.google.common.base.Predicates;
-import com.google.common.base.Throwables;
 import org.glassfish.jersey.client.ClientProperties;
 
-import javax.ws.rs.ProcessingException;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
 import java.net.URI;
 
@@ -24,14 +20,5 @@ public class BlobStoreJersey2ClientFactory extends AbstractBlobStoreJersey2Clien
 
     private BlobStoreJersey2ClientFactory(Client jerseyClient, URI endpoint) {
         super(new Jersey2EmoClient(jerseyClient), endpoint);
-    }
-
-    @Override
-    public boolean isRetriableException(Exception e) {
-        return super.isRetriableException(e) ||
-                (e instanceof WebApplicationException &&
-                        ((WebApplicationException) e).getResponse().getStatus() >= 500) ||
-                Throwables.getCausalChain(e).stream()
-                        .anyMatch(Predicates.instanceOf(ProcessingException.class)::apply);
     }
 }
