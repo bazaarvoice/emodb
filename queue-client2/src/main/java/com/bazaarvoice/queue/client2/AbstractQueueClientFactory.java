3dba15e81e6ab1e1caa60ffa3740cf3ddcefe3d3
ashwini-sheshadri <ashwini.sheshadri@bazaarvoice.com>
2022-08-11 16:49:52 -0700

    apikey validation - QueueClient

--- queue-client2/src/main/java/com/bazaarvoice/queue/client2/AbstractQueueClientFactory.java
+++ queue-client2/src/main/java/com/bazaarvoice/queue/client2/AbstractQueueClientFactory.java
@@ -1,5 +1,7 @@
 package com.bazaarvoice.queue.client2;
 
+import com.bazaarvoice.emodb.auth.InvalidCredentialException;
+import com.bazaarvoice.emodb.auth.util.ApiKeyEncryption;
 import com.bazaarvoice.emodb.client2.EmoClient;
 import com.bazaarvoice.emodb.common.jersey2.RetryPolicy;
 import com.bazaarvoice.emodb.queue.api.AuthQueueService;
@@ -7,6 +9,8 @@ import com.bazaarvoice.emodb.queue.api.QueueService;
 
 import java.net.URI;
 import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Abstract parent class for queue clients.  Subclasses are expected to create and configure an
@@ -23,6 +27,11 @@ abstract public class AbstractQueueClientFactory {
     }
 
     public QueueService usingCredentials(String apiKey) {
+        requireNonNull(apiKey, "API key is required");
+
+        if (ApiKeyEncryption.isPotentiallyEncryptedApiKey(apiKey)) {
+            throw new InvalidCredentialException("API Key is encrypted, please decrypt it");
+        }
         AuthQueueService authQueueService = new QueueClient(_endPoint, _client, RetryPolicy.createDefault());
         return new QueueServiceAuthenticatorProxy(authQueueService, apiKey);
     }
