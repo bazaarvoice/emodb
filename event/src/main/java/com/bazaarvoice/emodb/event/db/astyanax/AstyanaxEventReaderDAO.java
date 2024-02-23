70e2224bc47ffdf4ebf82d9206e849001de9a1d8
Mukesh SB <mukesh.sb@bazaarvoice.com>
2024-02-23 12:23:27 +0530

    RED 1215 (#816)
    * CL_ONE change
    
    * branch admin -prepare release emodb-6.5.69
    
    * branch admin -prepare for next development iteration
    
    * consistency changes
    
    * branch admin -prepare release emodb-6.5.69
    
    * version issue
    
    * version issue_yum
    
    * branch admin -prepare release emodb-6.5.71
    
    * branch admin -prepare for next development iteration
    
    * log_removal
    
    ---------
    
    Co-authored-by: jenkins <bv-jenkins@bazaarvoice.com>

--- event/src/main/java/com/bazaarvoice/emodb/event/db/astyanax/AstyanaxEventReaderDAO.java
+++ event/src/main/java/com/bazaarvoice/emodb/event/db/astyanax/AstyanaxEventReaderDAO.java
@@ -340,14 +340,19 @@ public class AstyanaxEventReaderDAO implements EventReaderDAO {
      */
     private Iterator<Column<ByteBuffer>> readManifestForChannel(final String channel, final boolean weak) {
         final ByteBuffer oldestSlab = weak ? _oldestSlab.getIfPresent(channel) : null;
-        final ConsistencyLevel consistency;
+        ConsistencyLevel consistency = ConsistencyLevel.CL_LOCAL_ONE ;//CL_LOCAL_QUORUM;;
 
         RangeBuilder range = new RangeBuilder().setLimit(50);
         if (oldestSlab != null) {
             range.setStart(oldestSlab);
-            consistency = ConsistencyLevel.CL_LOCAL_ONE;
         } else {
-            consistency = ConsistencyLevel.CL_LOCAL_QUORUM;
+
+            try{
+                String _sysConsistency = System.getProperty("read-consistency");
+                consistency = ConsistencyLevel.valueOf(_sysConsistency);
+            } catch(Exception e){
+                _log.debug("encountered exception while parsing ", e);
+            }
         }
 
         final Iterator<Column<ByteBuffer>> manifestColumns = executePaginated(
@@ -400,7 +405,7 @@ public class AstyanaxEventReaderDAO implements EventReaderDAO {
         // Using a lower consistency level could result in (a) duplicate events because we miss deletes and (b)
         // incorrectly closing or deleting slabs when slabs look empty if we miss adds.
         ColumnList<Integer> eventColumns = execute(
-                _keyspace.prepareQuery(ColumnFamilies.SLAB, ConsistencyLevel.CL_LOCAL_QUORUM)
+                _keyspace.prepareQuery(ColumnFamilies.SLAB, ConsistencyLevel.CL_LOCAL_ONE)
                         .getKey(slabId)
                         .withColumnRange(start, Constants.OPEN_SLAB_MARKER, false, Integer.MAX_VALUE));
 
