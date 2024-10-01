1bb525506ac7fb1ce75a2f9b3e1ad690711569ef
Rishu Yadav <rishu.yadav@bazaarvoice.com>
2024-10-01 10:44:51 +0530

    feat: include guava dependency injection for emodb

--- queue/src/main/java/com/bazaarvoice/emodb/queue/core/DefaultQueueService.java
+++ queue/src/main/java/com/bazaarvoice/emodb/queue/core/DefaultQueueService.java
@@ -4,7 +4,8 @@ import com.bazaarvoice.emodb.event.api.EventStore;
 import com.bazaarvoice.emodb.job.api.JobHandlerRegistry;
 import com.bazaarvoice.emodb.job.api.JobService;
 import com.bazaarvoice.emodb.queue.api.QueueService;
-import com.codahale.metrics.MetricRegistry;
+import com.bazaarvoice.emodb.queue.core.kafka.KafkaAdminService;
+import com.bazaarvoice.emodb.queue.core.kafka.KafkaProducerService;
 import com.google.inject.Inject;
 
 import java.time.Clock;
@@ -12,7 +13,7 @@ import java.time.Clock;
 public class DefaultQueueService extends AbstractQueueService implements QueueService {
     @Inject
     public DefaultQueueService(EventStore eventStore, JobService jobService, JobHandlerRegistry jobHandlerRegistry,
-                               Clock clock, MetricRegistry metricRegistry) {
-        super(eventStore, jobService, jobHandlerRegistry, MoveQueueJob.INSTANCE, clock, metricRegistry);
+                               Clock clock, KafkaAdminService adminService, KafkaProducerService producerService) {
+        super(eventStore, jobService, jobHandlerRegistry, MoveQueueJob.INSTANCE, clock,adminService, producerService);
     }
 }
