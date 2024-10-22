651df143d1648dc9c2ccc357a6ac35aed7b047ab
shan-1094 <ntyshaan@gmail.com>
2024-10-22 11:20:07 +0000

    PUT QAA API

---
+++ queue/src/main/java/com/bazaarvoice/emodb/queue/core/Entities/QueueExecutionAttributes.java
@@ -0,0 +1,100 @@
+package com.bazaarvoice.emodb.queue.core.Entities;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+import java.util.Objects;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class QueueExecutionAttributes {
+
+    private String queueName;
+
+    private String queueType;
+
+    private String topicName;
+    private String queueThreshold;
+    private String batchSize;
+    private String interval;
+
+    private String status;
+
+    public void setQueueName(String queueName) {
+        this.queueName = queueName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setQueueType(String queueType) {
+        this.queueType = queueType;
+    }
+
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+    }
+
+    public void setQueueThreshold(String queueThreshold) {
+        this.queueThreshold = queueThreshold;
+    }
+
+    public void setBatchSize(String batchSize) {
+        this.batchSize = batchSize;
+    }
+
+    public void setInterval(String interval) {
+        this.interval = interval;
+    }
+
+    public String getQueueThreshold() {
+        return queueThreshold;
+    }
+
+    public String getBatchSize() {
+        return batchSize;
+    }
+
+    public String getInterval() {
+        return interval;
+    }
+
+    public String getQueueName() {
+        return queueName;
+    }
+
+    public String getQueueType() {
+        return queueType;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public String getJsonPayload(QueueExecutionAttributes attributes) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(attributes);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof QueueExecutionAttributes)) return false;
+        QueueExecutionAttributes that = (QueueExecutionAttributes) o;
+        return Objects.equals(getQueueName(), that.getQueueName()) && Objects.equals(getQueueType(), that.getQueueType()) && Objects.equals(getTopicName(), that.getTopicName()) && Objects.equals(getQueueThreshold(), that.getQueueThreshold()) && Objects.equals(getBatchSize(), that.getBatchSize()) && Objects.equals(getInterval(), that.getInterval());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getQueueName(), getQueueType(), getTopicName(), getQueueThreshold(), getBatchSize(), getInterval());
+    }
+}
+
--- queue/src/main/java/com/bazaarvoice/emodb/queue/core/stepfn/StepFunctionService.java
+++ queue/src/main/java/com/bazaarvoice/emodb/queue/core/stepfn/StepFunctionService.java
@@ -3,14 +3,18 @@ package com.bazaarvoice.emodb.queue.core.stepfn;
 
 import com.amazonaws.services.stepfunctions.AWSStepFunctions;
 import com.amazonaws.services.stepfunctions.AWSStepFunctionsClientBuilder;
-import com.amazonaws.services.stepfunctions.model.StartExecutionRequest;
-import com.amazonaws.services.stepfunctions.model.StartExecutionResult;
-import com.amazonaws.services.stepfunctions.model.StateMachineDoesNotExistException;
-import com.amazonaws.services.stepfunctions.model.InvalidArnException;
-import com.amazonaws.services.stepfunctions.model.InvalidExecutionInputException;
-import com.amazonaws.services.stepfunctions.model.AWSStepFunctionsException;
+import com.amazonaws.services.stepfunctions.model.*;
+import com.bazaarvoice.emodb.queue.core.ssm.ParameterStoreUtil;
+import com.bazaarvoice.emodb.web.Entities.QueueExecutionAttributes;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.protocol.types.Field;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.MissingResourceException;
 
 /**
  * Production-level service to interact with AWS Step Functions using AWS SDK v1.
@@ -21,10 +25,13 @@ public class StepFunctionService {
 
     private final AWSStepFunctions stepFunctionsClient;
 
+    private final ParameterStoreUtil _parameterStoreUtil;
+
     /**
      * Constructor to initialize Step Function Client with AWS region and credentials.
      */
     public StepFunctionService() {
+        this._parameterStoreUtil = new ParameterStoreUtil();
         this.stepFunctionsClient = AWSStepFunctionsClientBuilder.standard()
                 .withRegion("us-east-1")
                 .build();
@@ -72,4 +79,76 @@ public class StepFunctionService {
             throw e; // Re-throw unexpected exceptions
         }
     }
+
+    public void stopActiveExecutions(String queueType, String queueName) {
+
+        try {
+            String activeExecutionARN = getActiveExecutionArn(queueType, queueName);
+            System.out.println("Stopping active execution: " + activeExecutionARN);
+
+            StopExecutionRequest stopRequest =  new StopExecutionRequest().withExecutionArn(activeExecutionARN);
+            stepFunctionsClient.stopExecution(stopRequest);
+            System.out.println("Stopped execution: " + activeExecutionARN);
+        } catch (Exception e) {
+            logger.error("Failure in stopping execution: {}", e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    public QueueExecutionAttributes getExistingSFNAttributes(String queueType, String queueName) throws JsonProcessingException {
+        String executionARN;
+        try {
+            executionARN = getActiveExecutionArn(queueType, queueName);
+
+            if(executionARN != null) {
+                logger.info("Fetching details for execution: " + executionARN);
+                DescribeExecutionRequest describeExecutionRequest = new DescribeExecutionRequest().withExecutionArn(executionARN);
+                DescribeExecutionResult describeExecutionResult = stepFunctionsClient.describeExecution(describeExecutionRequest);
+
+                String existingAttributes = describeExecutionResult.getInput();
+                logger.info("Fetched attributes for executionArn: " + executionARN + " => " + existingAttributes);
+
+                return new ObjectMapper().readValue(existingAttributes, QueueExecutionAttributes.class);
+            } else {
+                logger.info("No active executions found for queue_type: " + queueType + ", queue_name: " + queueName + " stateMachineARN: ");
+                return null;
+            }
+
+        } catch (Exception e) {
+            logger.error("Unexpected error in fetching sfn attributes for queue_type: " + queueType + ", queue_name: " + queueName);
+            throw e;
+        }
+    }
+
+    public String getActiveExecutionArn(String queueType, String queueName) {
+
+        // TODO: Extend this fetch part later based on queueType : queue/dedup/databus
+        // TODO: String universe = KafkaConfig::getUniverseFromEnv()  // add universe below
+        String stateMachineArn = _parameterStoreUtil.getParameter("/emodb/stepfn/stateMachineArn");   //databus/stateMachineArn
+
+        if(stateMachineArn == null || stateMachineArn.isEmpty()) {
+            throw new IllegalArgumentException("state machine arn can not be null/empty");
+        }
+
+        try {
+            ListExecutionsRequest listExecutionRequest = new ListExecutionsRequest().withStateMachineArn(stateMachineArn)
+                    .withStatusFilter(ExecutionStatus.RUNNING);
+
+            ListExecutionsResult listExecutionResults = stepFunctionsClient.listExecutions(listExecutionRequest);
+
+            if (!listExecutionResults.getExecutions().isEmpty()) {
+                String executionARN = listExecutionResults.getExecutions().get(0).getExecutionArn();
+                logger.info("Fetched executionARN: " + executionARN + " for stateMachineArn: " + stateMachineArn);
+                return executionARN;
+            } else {
+                logger.info("No active executions found for queue_type: " + queueType + ", queue_name: " + queueName + " stateMachineARN: " + stateMachineArn);
+                return null;
+            }
+        } catch (Exception e) {
+            logger.error("Unexpected error: {" + e.getMessage() + "} occurred while fetching active execution arn for queue_type: "+ queueType + ", queue_name: " + queueName + " ", e);
+            throw e;
+        }
+
+    }
+
 }
